a9d1414c5614812e12d6788618bdc124982067b6
Spec27 <shkvara27@gmail.com>
2022-01-15 23:08:10 +0200

    Готовое Дз 15.01.22

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtn = document.querySelector(".change-color");
+const bodyEl = document.querySelector("body");
+const currentColor = document.querySelector(".color");
+
+changeColorBtn.addEventListener("click", changeColor);
+function changeColor() {
+  const randomColor = (bodyEl.style.backgroundColor = `${getRandomHexColor()}`);
+  currentColor.textContent = randomColor;
+}
+
+/* Напиши скрипт,
+  который изменяет цвета фона элемента < body >
+  через инлайн стиль при клике на button.change - color 
+  и выводит значение цвета в span.color.
+Для генерации случайного цвета используй функцию getRandomHexColor. */
